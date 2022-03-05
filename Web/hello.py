98f42baa6801c9d6c8ef829b5e53ed82bcb33d9a
Park JuHan <1004sunye@naver.com>
2022-03-05 23:36:53 +0900

    Create hello.py

---
+++ Web/hello.py
@@ -0,0 +1,22 @@
+'''
+import requests # requests 라이브러리 설치 필요
+
+r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
+rjson = r.json()
+
+gus = rjson['RealtimeCityAir']['row']
+
+for gu in gus:
+    gu_name = gu['MSRSTE_NM']
+    print(gu_name)
+'''
+
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# 코딩 시작
